f6bc171aee223c8bc2146f103afdfae93861205a
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2022-10-22 16:04:35 -0300

    Added dependencies and Test

---
+++ src/test/java/ucu/edu/uy/AppTest.java
@@ -0,0 +1,17 @@
+package ucu.edu.uy;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+    @Before
+    public void init() {
+        // NOP()
+    }
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
